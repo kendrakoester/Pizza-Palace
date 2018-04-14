32bf4a5c5d777ca3e00384051ee954fbbf6fdfd3
KendraKoester <kendrakoester@mail.weber.edu>
2018-04-14 01:27:41 -0600

    Shopping Cart - addToCart functionality

--- js/cart.js
+++ js/cart.js
@@ -1,71 +1,58 @@
-
-//add to cart
-$(".add-to-cart").click(function (event) {
+$(".add-to-cart").click(function(event){
     event.preventDefault();
     var name = $(this).attr("data-name");
     var price = Number($(this).attr("data-price"));
     shoppingCart.addItemToCart(name, price, 1);
     displayCart();
 });
-
-//clear cart
-$("#clear-cart").click(function (event) {
+$("#clear-cart").click(function(event){
     shoppingCart.clearCart();
     displayCart();
 });
-
-//display cart on shopping cart page
 function displayCart() {
     var cartArray = shoppingCart.listCart();
-    console.log(cartArray);
+    //console.log(cartArray);
     var output = "";
     for (var i in cartArray) {
         output += "<li>"
-            + cartArray[i].name
-            + " <input class='item-count' type='number' data-name='"
-            + cartArray[i].name
-            + "' value='" + cartArray[i].count + "' >"
-            + " x " + cartArray[i].price
-            + " = " + cartArray[i].total
-            + " <button class='plus-item' data-name='"
-            + cartArray[i].name + "'>+</button>"
-            + " <button class='subtract-item' data-name='"
-            + cartArray[i].name + "'>-</button>"
-            + " <button class='delete-item' data-name='"
-            + cartArray[i].name + "'>X</button>"
-            + "</li>";
+            +cartArray[i].name
+            +" <input class='item-count' type='number' data-name='"
+            +cartArray[i].name
+            +"' value='"+cartArray[i].count+"' >"
+            +" x "+cartArray[i].price
+            +" = "+cartArray[i].total
+            +" <button class='plus-item' data-name='"
+            +cartArray[i].name+"'>+</button>"
+            +" <button class='subtract-item' data-name='"
+            +cartArray[i].name+"'>-</button>"
+            +" <button class='delete-item' data-name='"
+            +cartArray[i].name+"'>X</button>"
+            +"</li>";
     }
     $("#show-cart").html(output);
-    $("#count-cart").html(shoppingCart.countCart());
-    $("#total-cart").html(shoppingCart.totalCart());
+    $("#count-cart").html( shoppingCart.countCart() );
+    $("#total-cart").html( shoppingCart.totalCost() );
 }
-
-//remove an item from the shopping cart
-$("#show-cart").on("click", ".delete-item", function (event) {
+$("#show-cart").on("click", ".delete-item", function(event){
     var name = $(this).attr("data-name");
     shoppingCart.removeItemFromCartAll(name);
     displayCart();
 });
-
-//subtract one item from cart - dont remove entire item
-$("#show-cart").on("click", ".subtract-item", function (event) {
+$("#show-cart").on("click", ".subtract-item", function(event){
     var name = $(this).attr("data-name");
     shoppingCart.removeItemFromCart(name);
     displayCart();
 });
-
-//add item to cart
-$("#show-cart").on("click", ".plus-item", function (event) {
+$("#show-cart").on("click", ".plus-item", function(event){
     var name = $(this).attr("data-name");
     shoppingCart.addItemToCart(name, 0, 1);
     displayCart();
 });
-
-//total count of item
-$("#show-cart").on("change", ".item-count", function (event) {
+$("#show-cart").on("change", ".item-count", function(event){
     var name = $(this).attr("data-name");
     var count = Number($(this).val());
     shoppingCart.setCountForItem(name, count);
     displayCart();
 });
 
+displayCart();
